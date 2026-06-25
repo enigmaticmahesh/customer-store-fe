6e26107effa47798e5d3346bfce001dcdb015fd8
enigmaticmahesh <challengermahesh@gmail.com>
2026-06-25 16:41:32 +0530

    auto refresh lint issue fix config

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,14 +1,6 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { createRootRoute } from "@tanstack/react-router";
+import RootComponent from "./-components/root.component";
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
-
-function RootComponent() {
-  return (
-    <React.Fragment>
-      <Outlet />
-    </React.Fragment>
-  )
-}
+});
